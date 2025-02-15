66c83de628703527f3e111572b3dc36628f5c70c
0xDEADBEEF <programmer0415@gmail.com>
2025-02-15 20:18:24 +0900

    Chore: Init Titan Project
    - 타이탄 프로젝트 초기 설정 진행.

--- DummyClient/pch.h
+++ DummyClient/pch.h
@@ -13,4 +13,8 @@
 #include "CorePch.h"
 
 
-#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+#ifdef _DEBUG
+#pragma comment( lib, "Debug\\Titan.lib")
+#else
+#pragma comment( lib, "Release\\Titan.lib")
+#endif
---
+++ Titan/Types.h
@@ -0,0 +1,21 @@
+/**********************************************************************************************************************
+* @file  Types.h
+*
+* @brief 타입 정의 헤더 파일 
+*
+* @date  2025.02.15
+**********************************************************************************************************************/
+
+
+#pragma once
+
+
+using TByte   = unsigned char;
+using TUInt8  = unsigned __int8;
+using TUInt16 = unsigned __int16;
+using TUInt32 = unsigned __int32;
+using TUInt64 = unsigned __int64;
+using TInt8   = __int8;
+using TInt16  = __int16;
+using TInt32  = __int32;
+using TInt64  = __int64;
